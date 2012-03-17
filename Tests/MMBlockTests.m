dd37284b1fa44805b034f3f5a3708f854b4a50bb
Matt Diephouse <matt@diephouse.com>
2012-03-17 07:45:05 -0400

    Improve code block support: a block can contain empty lines

---
+++ Tests/MMBlockTests.m
@@ -0,0 +1,13 @@
+//
+//  MMBlockTests.m
+//  MMMarkdown
+//
+//  Created by Matt Diephouse on 3/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MMBlockTests.h"
+
+@implementation MMBlockTests
+
+@end
